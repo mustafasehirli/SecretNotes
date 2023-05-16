93e24750f0b176f700a4b87ebe034cae04086ca3
mustafasehirli <mustafaasehirli@gmail.com>
2023-05-16 22:00:04 +0300

    onclick_save_button added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tkinter
 from PIL import Image, ImageTk
+from cryptography.fernet import Fernet
+
 
 window = tkinter.Tk()
 window.minsize(width=400, height=700)
@@ -10,7 +12,15 @@ image = ImageTk.PhotoImage(open_image)
 
 # onclick save button
 def onclick_save_button():
-    pass
+    title = note_title_entry.get()
+    key = Fernet.generate_key()
+    fernet = Fernet(key)
+    data_text = note_text.get(1.0, "end")
+    encrypted = fernet.encrypt(bytes(data_text, "utf-8"))
+    with open(f"{title}.txt", mode="wb") as file:
+        file.write(encrypted)
+    note_title_entry.delete(0, "end")
+    note_text.delete(1.0, "end")
 
 
 # onclick fetch button
@@ -23,26 +33,27 @@ image_label = tkinter.Label(image=image)
 image_label.config(width=75, height=75)
 image_label.pack()
 
-# note title
-password_title = tkinter.Label()
-password_title.config(text="Note Title", pady=10, padx=20)
-password_title.pack()
+# # note title
+note_title = tkinter.Label()
+note_title.config(text="Note Title", pady=10, padx=20)
+note_title.pack()
 
 # Entry
-password_entry = tkinter.Entry()
-password_entry.pack()
+note_title_entry = tkinter.Entry()
+note_title_entry.pack()
 
-# note text title
-note_text = tkinter.Label()
-note_text.config(text="Note Text", pady=10, padx=20)
-note_text.pack()
+# # note text title
+note_title_text = tkinter.Label()
+note_title_text.config(text="Note Text", pady=10, padx=20)
+note_title_text.pack()
+
 
 # text
-text = tkinter.Text()
-text.config(width=30, height=10, pady=30)
-text.pack()
+note_text = tkinter.Text()
+note_text.config(width=30, height=10)
+note_text.pack()
 
-# password title
+# # password title
 password_title = tkinter.Label()
 password_title.config(text="Password Title", pady=10, padx=20)
 password_title.pack()
