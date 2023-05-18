77cea4a895b80f38458f3724f9a337c42feb1ba5
mustafasehirli <mustafaasehirli@gmail.com>
2023-05-18 08:54:38 +0300

    the end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import tkinter
+from tkinter import messagebox
 from PIL import Image, ImageTk
 import base64
 
@@ -11,32 +12,66 @@ open_image = Image.open("topsecret.png")
 image = ImageTk.PhotoImage(open_image)
 
 
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+
+
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc).decode()
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+
+
 # onclick save button
 def onclick_save_button():
     title = note_title_entry.get()
-    data = note_text.get(1.0, "end")
+    message = note_text.get(1.0, "end")
+    password = password_entry.get()
 
-    data_bytes = data.encode("utf-8")
-    base64_bytes = base64.b64encode(data_bytes)
-    base64_string = base64_bytes.decode('utf-8')
+    if title == "" or message == "" or password == "" :
+        tkinter.messagebox.showerror(title="Error", message="Please enter all info")
 
-    with open(f"{title}.txt", mode="wb") as file:
-        file.write(base64_string.encode('utf-8'))
-    file.close()
+    else:
+        message_encrypted = encode(password, message)
 
-    note_title_entry.delete(0, "end")
-    note_text.delete(1.0, "end")
+        try:
+            with open(f"MyText.txt", mode="a") as file:
+                file.write(f"\n{title}\n{message_encrypted}")
+            file.close()
+        except FileNotFoundError:
+            with open(f"MyText.txt", mode="w") as file:
+                file.write(f"\n{title}\n{message_encrypted}")
+            file.close()
+        finally:
+            note_title_entry.delete(0, "end")
+            note_text.delete(1.0, "end")
+            password_entry.delete(0, "end")
 
 
 # onclick fetch button
 def onclick_fetch_button():
-    base64_string = note_text.get(1.0, "end")
-    base64_bytes = base64_string.encode('utf-8')
-    data_bytes = base64.b64decode(base64_bytes)
-    data = data_bytes.decode('utf-8')
-    note_text.delete("1.0", "end")
-    for i in data:
-        note_text.insert("end", i)
+    message = note_text.get(1.0, "end")
+    password = password_entry.get()
+
+    if message == "" or password == "" :
+        tkinter.messagebox.showerror(title="Error", message="Please enter all info")
+
+    else:
+        try:
+            message_encrypted = decode(password, message)
+            note_text.delete(1.0, "end")
+            note_text.insert("1.0", message_encrypted)
+        except:
+            tkinter.messagebox.showerror(title="Error", message="Please make sure of encrypted info.")
 
 
 # image
